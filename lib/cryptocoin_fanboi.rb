27676d0a1e80489a19781315e532dba10ed0fa7a
Your Name <you@example.com>
2018-01-01 18:29:16 +0000

    feature: Coins can now be select by symbol using the named keyword *watch*.

--- lib/cryptocoin_fanboi.rb
+++ lib/cryptocoin_fanboi.rb
@@ -9,31 +9,44 @@ require 'table-formatter'
 
 class CryptocoinFanboi
 
-  def initialize()
+  def initialize(watch: [])
 
+    @watch = watch.map(&:upcase)
+    
     @tfo = TableFormatter.new
     @tfo.labels = %w(Name USD BTC) + ['% 24hr:', '% 1 week:']
+    @coins = fetch_coinlist(watch: @watch)
 
   end
-
-  def to_s(limit: 5, markdown: false)
-    
-    @tfo.source = fetch_coinlist limit: limit
-    @tfo.display markdown: markdown
-
+  
+  def coin_abbreviations()
+    @coins.map {|x| "%s (%s)" % [x['name'], x['symbol']] }
   end
+  
+  alias abbreviations coin_abbreviations
 
-  private
-
-  def fetch_coinlist(limit: 5)
-
-    coins = JSON.parse(Coinmarketcap.coins.body).take(limit).map do |coin|
+  def to_s(limit: nil, markdown: false)
+        
+    coins = fetch_coinlist(limit: limit, watch: @watch).map do |coin|
 
       %w(name price_usd price_btc percent_change_24h percent_change_7d)\
         .map {|x| coin[x] }
 
     end
 
+    @tfo.source = coins
+    @tfo.display markdown: markdown
+
+  end
+
+  private
+
+  def fetch_coinlist(limit: nil, watch: [])
+
+    a = JSON.parse(Coinmarketcap.coins.body)    
+    coins = watch.any? ? a.select {|x| watch.include? x['symbol']} : a    
+    limit ? coins.take(limit) : coins
+    
   end
 
 end
